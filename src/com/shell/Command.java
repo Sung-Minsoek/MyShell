cf898f057b70aa4949333806c6602192b44a08c2
성민석 <admin@seongminseog-ui-MacBookPro.local>
2025-05-22 15:40:58 +0900

    FileManager.java Command.java: Add File Managing interface.   - Add FileWrite / FileRead method and home directory of user in interface.   - Command class implements these methods.   - The exception of FileIO is handled to caller method of Command class.     Because, Command class can be other commands like "echo" or somethings.     So it is sure that the command handles the methods' exceptions.

--- src/com/shell/Command.java
+++ src/com/shell/Command.java
@@ -1,8 +1,12 @@
 package com.shell;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.FileReader;
+
 import com.shell.env.*;
 
-public class Command {
+public class Command implements FileManager {
 	protected EnvironmentVariable envs;
 	
 	public Command(EnvironmentVariable envs) {
@@ -22,5 +26,48 @@ public class Command {
 	
 	public void execute(String[] args) {
 		return;
+	}
+
+	@Override
+	public void FileWrite(String input, String fileName) throws Exception {
+		String filePath = home + "/" + fileName;		
+		
+		try {
+			File file = new File(filePath);
+			FileWriter fw = new FileWriter(file);
+			
+			fw.write(input);
+			fw.close();
+		}
+		
+		catch (Exception e) {
+			System.out.println(e);
+			throw e;
+		}
+	}
+
+	@Override
+	public String FileRead(String fileName) throws Exception {
+		String filePath = home + "/" + fileName;
+		String output = "";
+		
+		try {
+			File file = new File(filePath);
+			FileReader fr = new FileReader(file);
+			
+			int i = 0;
+			
+			while((i = fr.read()) != -1) {
+				output += (char) i;
+			}
+			
+			fr.close();
+		} 
+		
+		catch(Exception e) {
+			throw e;
+		}
+		
+		return output;
 	}
 }
